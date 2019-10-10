52fb5386a232950a852ae78ac85a6c34dd68b77a
Peter Hardy <peterphardy@gmail.com>
2019-10-09 19:17:08 -0700

    slider section done, onto about section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // $(document).ready method
 $(document).ready(function() {
-  // activates superslides code
+  // activates superslides code with jQuery
   $('#slides').superslides({
     // fade quality when switching images
     animation: "fade",
@@ -8,6 +8,15 @@ $(document).ready(function() {
     play: 5000,
     // takes away user manually changing images
     pagination: false
+  });
+  // Creates typed effect in supersliders section
+  var typed = new Typed(".typed", {
+    strings: ["Software Developer.", "Database Administrator.", "Programmer."],
+    typeSpeed: 65,
+    loop: true,
+    // wait before it starts typing again
+    startDelay: 500,
+    showCursor: false
   });
   
 });
